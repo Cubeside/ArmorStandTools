1d429b1e0e4f5a5177d181f2bf09765a7a16643a
Brokkonaut <hannos17@gmx.de>
2018-09-17 06:16:50 +0200

    Fix world guard permissions check

--- src/main/java/de/iani/ast/WorldGuardProtectionListener.java
+++ src/main/java/de/iani/ast/WorldGuardProtectionListener.java
@@ -1,12 +1,16 @@
 package de.iani.ast;
 
 import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.internal.permission.RegionPermissionModel;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.Flags;
 
@@ -22,9 +26,18 @@ public class WorldGuardProtectionListener implements Listener {
     @EventHandler
     public void onArmorStandTeleport(ArmorStandTeleportEvent event) {
         ApplicableRegionSet set = WorldGuard.getInstance().getPlatform().getRegionContainer().createQuery().getApplicableRegions(BukkitAdapter.adapt(event.getLocation()));
-        if (!set.testState(worldGuard.wrapPlayer(event.getPlayer()), Flags.BUILD)) {
+        if (!canBuild(event.getPlayer(), set)) {
             event.getPlayer().sendMessage(ChatColor.BLUE + "[AST] " + ChatColor.DARK_RED + "Keine Baurechte!");
             event.setCancelled(true);
         }
+    }
+
+    private boolean canBuild(Player bplayer, ApplicableRegionSet set) {
+        LocalPlayer player = worldGuard.wrapPlayer(bplayer);
+        return hasBypass(player, bplayer.getWorld()) || set.testState(player, Flags.BUILD);
+    }
+
+    private boolean hasBypass(LocalPlayer wgPlayer, World world) {
+        return new RegionPermissionModel(wgPlayer).mayIgnoreRegionProtection(BukkitAdapter.adapt(world));
     }
 }
